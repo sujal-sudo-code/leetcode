52cfb9515ab243a0c4080bc45bf925daa332557d
Sujal Rathore <work.sujalrathore@gmail.com>
2026-08-12 23:29:58 +0530

    Time: 4 ms (99.06%) | Memory: 47.6 MB (68.40%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return palindrome(s, l + 1, r) || palindrome(s, l, r - 1);
+            }
+
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+
+    boolean palindrome(String s, int l, int r) {
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
